3e3524512a7e30bf60a203630b51e24687d2d6bb
David HZ <davhuzh@gmail.com>
2022-04-01 16:38:58 +0800

    [refactor] Shifted nvim configs to its own folder

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
-ln -s "$PWD/.vimrc" ~/.vimrc
+#!/bin/sh
+echo "Installing dotfiles..."
+echo "Installing neovim configurations..."
+ln -s "$PWD/nvim/.vimrc" ~/.vimrc
 mkdir ~/.config/nvim
-ln -s "$PWD/init.vim" ~/.config/nvim/init.vim
+ln -s "$PWD/nvim/init.vim" ~/.config/nvim/init.vim
+echo "Finished installation of dotfiles"
+
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,6 @@
+#!/bin/sh
+echo "Uninstalling dotfiles..."
+echo "Uninstalling neovim configurations..."
 rm ~/.vimrc
 rm -r ~/.config/nvim
+echo "Finished uninstalling dotfiles"
