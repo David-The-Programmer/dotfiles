8eb00823c7ef87ba673793df8839368deba156e0
David-The-Programmer <davhuzh@gmail.com>
2022-04-10 14:22:36 +0800

    [feat] Uninstall script removes all plugin dirs

--- nvim/uninstall.sh
+++ nvim/uninstall.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# Remove the symlink to nvim subfolder in repo
 rm -rf ~/.config/nvim
+
+# Remove all plugins installed via paq
+rm -rf "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/
