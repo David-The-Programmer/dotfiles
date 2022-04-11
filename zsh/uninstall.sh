d73c8dd74fd958c05761682133e3ff71b9c1171b
David-The-Programmer <davhuzh@gmail.com>
2022-04-11 13:58:38 +0800

    [feat] Uninstall ohmyzsh & powerlevel 10k by default

--- zsh/uninstall.sh
+++ zsh/uninstall.sh
@@ -4,11 +4,11 @@
 rm -f ~/.zshrc
 rm -f ~/.p10k.zsh
 
-# Uncomment the following line to uninstall Powerlevel10k theme from Oh My Zsh
-# rm -rf -- ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# Comment the following line to disable the uninstallation of the Powerlevel10k theme from Oh My Zsh
+rm -rf -- ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# Uncomment the following line to uninstall Oh My Zsh
-# rm -rf ~/.oh-my-zsh
+# Comment the following line to disable the uninstallation of Oh My Zsh
+rm -rf ~/.oh-my-zsh
 
 # restart zsh
 exec zsh
