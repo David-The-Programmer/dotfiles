8a97628743fe40a759034b8fab78e30d9d5957b3
David-The-Programmer <davhuzh@gmail.com>
2022-04-13 13:29:24 +0800

    [feat] Remap for navigation on wrapped lines

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -4,12 +4,16 @@
 
 local map = vim.api.nvim_set_keymap
 
+-- Set the leader key to space
+vim.g.mapleader = " "
+
 -- Remap for shifting between tabs
 map("n", "Q", "gT", { noremap = true })
 map("n", "W", "gt", { noremap = true })
 
--- Set the leader key to space
-vim.g.mapleader = " "
+-- Remap to allow navigation on wrapped line 
+map("n", "j", "gj", { noremap = true })
+map("n", "k", "gk", { noremap = true })
 
 -- Keymaps for NERD Commenter plugin
 -- Invoke comments by pressing ctrl /
