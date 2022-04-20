f9bc690c908b7bf6e80c1a199215635100434834
David-The-Programmer <davhuzh@gmail.com>
2022-04-20 14:18:11 +0800

    [refactor] Moved all plugin specific keymaps to respective config files

--- nvim/lua/plugins/configs/nerd-commenter.lua
+++ nvim/lua/plugins/configs/nerd-commenter.lua
@@ -7,3 +7,7 @@ vim.g["NERDSpaceDelims"] = 1
 
 -- Allow commenting and inverting empty lines (useful when commenting a region)
 vim.g["NERDCommentEmptyLines"] = 1
+
+local map = vim.api.nvim_set_keymap
+-- Invoke comments by pressing ctrl /
+map("", "<C-_>", "<plug>NERDCommenterToggle", { noremap = false })
