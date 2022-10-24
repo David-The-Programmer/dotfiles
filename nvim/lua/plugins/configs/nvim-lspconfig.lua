edcb82563d23c02b06b4902e44cfdd58f7f0adfb
David-The-Programmer <davhuzh@gmail.com>
2022-10-24 13:26:00 +0800

    [fix] Fixed deprecation error for nvim-lspconfig

--- nvim/lua/plugins/configs/nvim-lspconfig.lua
+++ nvim/lua/plugins/configs/nvim-lspconfig.lua
@@ -25,7 +25,7 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dl", "<cmd>Telescope diagnostics<CR>", opts)
 end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 return {
     on_attach = on_attach,
